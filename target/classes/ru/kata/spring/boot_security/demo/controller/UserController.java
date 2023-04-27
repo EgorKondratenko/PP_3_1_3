0e229b0e1cef7a6c26704d19f5dbf05d1a2eabe1
EgorKondratenko <122119909+EgorKondratenko@users.noreply.github.com>
2023-04-27 18:09:00 +0300

    Add files via upload

---
+++ target/classes/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,29 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userServiceImp;
+
+    @Autowired
+    public UserController(UserService userServiceImp) {
+        this.userServiceImp = userServiceImp;
+    }
+
+    @GetMapping()
+    public String getUser(Model model, Principal principal) {
+        User user = userServiceImp.findByName(principal.getName());
+        model.addAttribute("user", user);
+        return "user";
+    }
+}
